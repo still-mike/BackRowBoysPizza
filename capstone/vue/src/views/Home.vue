5455cd3a5b081f69378ce79e47279ae30c923831
Jonathan Fettig <jonathanpfettig@gmail.com>
2022-08-08 19:48:07 -0400

    first edits

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h2>Welcome to Back Row Boyz Pizza</h2>
+    <p>We will be with you shortly!</p>
   </div>
 </template>
 
