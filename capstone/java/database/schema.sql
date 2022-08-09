e84bf31550c33644f81df1a6f04fd849d0ea6b80
Jonathan Fettig <jonathanpfettig@gmail.com>
2022-08-09 09:02:39 -0400

    initial database schema scripted

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, pizza_ingredients, pizzas, ingredients, orders;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +9,48 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE ingredients(
+    id SERIAL,
+    ingredient_name varchar(50) NOT NULL UNIQUE,
+    tier varchar(50) NOT NULL,
+    available boolean NOT NULL,
+    CONSTRAINT PK_ingredients PRIMARY KEY (id)
+);
+
+CREATE TABLE orders(
+    id SERIAL UNIQUE,
+    order_status varchar(50) NOT NULL,
+    is_delivery boolean NOT NULL,
+    employee_name varchar(50),
+    order_time TIMESTAMP,
+    cust_address varchar(50),
+    cust_email varchar(50) NOT NULL
+);
+
+CREATE TABLE pizzas(
+    id SERIAL,
+    pizza_size varchar(50) NOT NULL,
+    dough varchar(50) NOT NULL,
+    shape varchar(50) NOT NULL,
+    sauce_type varchar(50) NOT NULL,
+    order_id int,
+    CONSTRAINT PK_pizzas PRIMARY KEY (id),
+    CONSTRAINT fk_order
+        FOREIGN KEY (order_id)
+		REFERENCES orders(id)
+);
+
+CREATE TABLE pizza_ingredients (
+    pizza_id int,
+    ingredient_id int,
+    CONSTRAINT fk_pizza
+        FOREIGN KEY (pizza_id)
+		 REFERENCES pizzas(id),
+    CONSTRAINT fk_ingredient
+        FOREIGN KEY (ingredient_id)
+		 REFERENCES ingredients(id)
+);
+
 
 COMMIT TRANSACTION;
