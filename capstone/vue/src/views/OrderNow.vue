1da33b9bb22841f496dd9fb6171280601079399f
Mike Still <mikestill12@gmail.com>
2022-08-09 15:26:01 -0400

    trying to get a pizza-size-list working

---
+++ capstone/vue/src/views/OrderNow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="pizza-size-app">
+    <pizza-size-list></pizza-size-list>
+  </div>
+</template>
+
+<script>
+import PizzaSizeList from './components/PizzaSizeList';
+
+export default {
+  name: 'order-now',
+  components: {
+    PizzaSizeList
+  }
+}
+</script>
+
+<style></style>
