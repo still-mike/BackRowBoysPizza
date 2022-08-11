40cad8411ac254a2815b86b11381dc0154324874
Matthew Nowell <matthewnowell97@gmail.com>
2022-08-11 18:13:58 -0400

    ImNotEnjoyingThisProject...ContactUs

---
+++ capstone/vue/src/components/ContactDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h4>{{title}}</h4>
+  <div>
+       <a href="mailto:backrowboyzpizza@gmail.com" class="btn-container" v-if="typeOfAddress==='email'">{{address}}</a>
+       <p v-if="typeOfAddress==='phone'">{{address}}</p>
+       <a href="https://www.yelp.com/backrowboyzpizza" v-if="typeOfAddress ==='review'">{{address}}</a>
+       
+
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "contact-details",
+    props: [
+        "title", "address", "typeOfAddress"
+    ]
+
+}
+</script>
+
+<style>
+
+</style>
--- capstone/vue/src/views/ContactUs.vue
+++ capstone/vue/src/views/ContactUs.vue
@@ -1,13 +1,68 @@
 <template>
-  <h1>contact us</h1>
+  <div class="contact">
+  <h1 class="contactHeader">contact us</h1>
+  <div class="firstRow">
+  <contact-details class="contactInfo" :typeOfAddress="'phone'" :title="'call us'" :address="'(484)-803-8318'"/>
+  <contact-details class="contactInfo" :typeOfAddress="'email'" :title="'email us'" :address="'backrowboyzpizza@gmail.com'"/>
+  </div>
+  <div class="secondRow">
+  <contact-details :title="'leave us a review!'" :typeOfAddress="'review'"  :address="'yelp.com/backrowboyzpizza'"/>
+  </div>
+  </div>
 </template>
 
 <script>
+import ContactDetails from "../components/ContactDetails.vue"
 export default {
+  components: {
+    ContactDetails
+  }
+
 
 }
 </script>
 
 <style>
+.firstRow {
+  display: flex;
+  text-align: center;
+  justify-content: center;
 
+  
+}
+.firstRow div {
+  margin: 1em;
+  background-color: #FAF9F6;
+  text-align: center;
+}
+
+.contactInfo {
+  width: 30%;
+}
+
+.secondRow {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  
+}
+
+.secondRow div {
+  background-color: #FAF9F6;
+  width: 69%;
+  text-align: center;
+  margin: auto;
+  padding-bottom: 0.5em;
+}
+.contactHeader {
+  text-align: center;
+  margin-top: 2em;
+}
+.contact a {
+  text-decoration: none;
+  color: #F76C4B;
+}
+.contact a:visited {
+  color: #F76C4B;
+}
 </style>
