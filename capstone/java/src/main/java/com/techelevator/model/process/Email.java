232a13c2af2e1ab8eca9092caf85f55f16ef7ad1
Jonathan Fettig <jonathanpfettig@gmail.com>
2022-08-13 18:18:11 -0400

    attempting to add sendgrid email service

---
+++ capstone/java/src/main/java/com/techelevator/model/process/Email.java
@@ -0,0 +1,7 @@
+package com.techelevator.model.process;
+
+public class Email {
+    private String
+    private String
+    private String
+}
---
+++ capstone/java/src/main/java/com/techelevator/services/emailService.java
@@ -0,0 +1,7 @@
+package com.techelevator.services;
+
+public interface emailService {
+    // need an email model like CatPic?
+    Email sendEmail();
+
+}
