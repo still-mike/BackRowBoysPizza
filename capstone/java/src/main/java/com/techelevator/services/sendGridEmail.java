546238cbdfd745053f299aa67e96cfc4f39c00f9
Jonathan Fettig <jonathanpfettig@gmail.com>
2022-08-14 10:02:07 -0400

    more sendGrid Dynamic testing

--- capstone/java/src/main/java/com/techelevator/services/sendGridEmail.java
+++ capstone/java/src/main/java/com/techelevator/services/sendGridEmail.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 
 
 public class sendGridEmail implements email{
-//todo - make this a regular method by changing the name and giving it the parameter we want to pass in: subject, to, content
+//todo - make this a regular method by changing the name and giving it the parameters we want to pass in: subject, to, content
     public static void main(String[] args) throws IOException {
         Email from = new Email("backrowboyzpizza@gmail.com");
-        String subject = "This email is from our java server";
-        Email to = new Email("mikestill12@gmail.com");
-        Content content = new Content("text/plain", "Hi Mike, how does it feel to get an email from your backend server? - Jonathan  ");
+
+        String subject = "after dynamic test - This email is from our java server - test for sendGrid";
+        Email to = new Email("Jonathanpfettig@gmail.com");
+        Content content = new Content("text/plain", "Hi Jonathan, how does it feel to get an email from your backend server? - Jonathan - test 3 bof the dynamic  ");
         Mail mail = new Mail(from, subject, to, content);
+
+        //This is the template I created online at https://mc.sendgrid.com/dynamic-templates:
+        //        mail.setTemplateId("d-1a77882e59154014a3ca8870a98c3ff1");
+        // it broke the basic text email though
 
         SendGrid sg = new SendGrid("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");
         Request request = new Request();
