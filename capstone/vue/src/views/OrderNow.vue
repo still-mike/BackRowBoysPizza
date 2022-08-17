509fa349bbe081baed3e47230707759ddc134886
Matthew Nowell <matthewnowell97@gmail.com>
2022-08-17 16:56:54 -0400

    Uniformity On OrderNow

--- capstone/vue/src/views/OrderNow.vue
+++ capstone/vue/src/views/OrderNow.vue
@@ -3,8 +3,9 @@
   <div id="capsule">
     <website-header id="Header"/>
     <div>
-    <h1>order now</h1>
-
+    <h1 id="orderNow">order now</h1>
+    <div id="tomatoBG">
+    </div>
     <div class = 'RouterPhoto'>
       <router-link :to="{name: 'orderNowCustomPizzaV'}"><img src="../assets/cyo_no_border.png" class="MidPageLinks"> </router-link>
       <router-link :to="{name: 'orderNowSpecialtyPizzaV'}"><img src="../assets/sp_no_border.png" class="MidPageLinks"></router-link>
@@ -80,9 +81,11 @@ div#capsule {
     height: 200px;
     background-color: #F76C4B;
     border-radius: 60px;
-    border: solid rgb(5, 0, 0) 2px;
+    border: solid rgb(5, 0, 0) 0px;
     color: white;
     cursor: pointer;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+  
 }
 
 .RouterPhoto {
@@ -95,5 +98,12 @@ div#capsule {
     transform: translate(-50%, -50%);
     -ms-transform: translate(-50%, -50%);
 }
-
+#tomatoBG {
+  background-image: URL("https://images.unsplash.com/photo-1624821588855-a3ffb0b050ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+  height: 520px;
+  padding: 100px;
+}
+#orderNow{
+  text-align: -webkit-center;
+}
 </style> 
